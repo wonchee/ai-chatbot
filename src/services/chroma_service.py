1bd99ffcc1aa58566ff8ccdd4fbcc294db4f6e34
wonchee <wwchee1604@gmail.com>
2024-10-10 21:57:35 +0800

    Added pysqlite3 import

--- src/services/chroma_service.py
+++ src/services/chroma_service.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from typing import List
 from langchain.schema.document import Document
 from langchain_chroma import Chroma
